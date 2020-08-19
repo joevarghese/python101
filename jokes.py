87ad5c9b63acbca780cfa77fee2460f884c96da7
Joe Varghese <joevarghese21@Gmail.com>
2020-08-18 22:18:38 -0500

    added hello.txt

---
+++ jokes.py
@@ -0,0 +1,19 @@
+name = input("What's your name?")
+greeting =  f"hello, {name}"
+
+print(greeting)
+
+name1 = input("What's your name?")
+greeting = f"Hello, {name1}"
+test = len(name1)
+
+print(name1.upper())
+print(f"You have {test} letters in your name")
+
+print("Please fill in the blanks below:")
+print("____(name)____'s favorite subject in school is ____(subject)____.")
+
+name3 = input("what's your name")
+subject = input("What's your favorite subject")
+
+
